74ff1cb8f82ffd97541672f8d46708d2d9b11e6b
Lusai <ellipse120@gmail.com>
2017-05-09 19:57:03 +0800

    db.js line17, add err var

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -14,7 +14,7 @@ mongoose.connect(dbURI);
 mongoose.connection.on('connected', function(){
 	console.log('Mongoose connected to ' + dbURI);
 });
-mongoose.connection.on('error', function(){
+mongoose.connection.on('error', function(err){
   console.log('Mongoose connection error: ' + err);
 });
 mongoose.connection.on('disconnected', function(){
